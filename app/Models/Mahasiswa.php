152f2c95fafb4b5746a88ee49b2cf18f990e6361
Thomas Aquino <thomasaquino746@email.com>
2026-06-23 18:20:15 +0700

    Add fillable fields to mahasiswa model

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'jurusan',
+        'email',
+        'alamat',
+    ];
 }
