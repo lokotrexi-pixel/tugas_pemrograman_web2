faba022e9477cd589a76b1b7a6917962a8b9f15d
lokotrexi-pixel <lokotrexi@gmail.com>
2026-06-23 23:12:56 +0700

    Revert "Database"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,14 +3,16 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Mahasiswa;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
     public function run()
     {
-        $this->call([
-            MahasiswaSeeder::class,
-        ]);
+        // \App\Models\User::factory(10)->create();
     }
 }
